046cb05b740392cd8560b9dd0d49c836b92245c9
neouuuu <nallende@alumnos.exa.unicen.edu.ar>
2026-08-23 12:01:34 -0300

    descripcion de la app

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -9,5 +10,6 @@ const ruta_media = "./static"
 func main() {
 	fs := http.FileServer(http.Dir(ruta_media))
 	http.Handle("/", fs)
+	fmt.Printf("Andando")
 	http.ListenAndServe(":8080", nil)
 }
